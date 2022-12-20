ffb973cc6733a039ad6e6baa19007fa5d05cf21e
Jerry <gch02933@naver.com>
2022-12-20 17:21:17 +0900

    feat: notice 생성

---
+++ controllers/notice.controller.js
@@ -0,0 +1,60 @@
+const items = [
+    {
+      subject: "첫번째 게시물",
+      content: "content",
+      name: "name...",
+    },
+  ];
+  
+  exports.index = (req, res) => {
+    res.render("notice/index.html");
+  };
+  
+  exports.list = (req, res) => {
+    res.render("notice/list.html", { items });
+  };
+  
+  exports.getWrite = (req, res) => {
+    res.render("notice/write.html");
+  };
+  
+  exports.postWrite = (req, res) => {
+    const { content, subject, name } = req.body;
+    items.push({ content, subject, name });
+    res.redirect(`view?index=${items.length - 1}`);
+  };
+  
+  exports.view = (req, res) => {
+    const { index } = req.query;
+    const item = {
+      ...items[index],
+      index,
+    };
+    res.render("notice/view.html", { item });
+  };
+  
+  exports.getModify = (req, res) => {
+    const { index } = req.query;
+    const item = {
+      ...items[index],
+      index,
+    };
+  
+    res.render("notice/modify.html", { item });
+  };
+  
+  exports.postModify = (req, res) => {
+    const { index, subject, content, name } = req.body;
+    items[index].subject = subject;
+    items[index].content = content;
+    items[index].name = name;
+  
+    res.redirect(`view?index=${index}`);
+  };
+  
+  exports.delete = (req, res) => {
+    const { index } = req.query;
+    items.splice(index, 1);
+    res.redirect("list");
+  };
+  
---
+++ routes/notice.route.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/notice.controller')
+
+router.get("/", controller.index);
+router.get("/list", controller.list);
+router.get("/write", controller.getWrite);
+router.post("/write", controller.postWrite);
+router.get("/view", controller.view);
+router.get("/modify", controller.getModify);
+router.post("/modify", controller.postModify);
+router.get("/delete", controller.delete);
+
+module.exports = router;
